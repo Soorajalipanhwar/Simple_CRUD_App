465a9ab0e11f8008d78b3d0c76a99631312127d6
soorajalipanhwar <Soorajalipanhwar007@gmail.com>
2024-09-30 23:29:35 +0500

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace Simple_CRUD_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(395, 33);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(921, 371);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "quantity";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 143);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 619);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add Sales";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Item ID";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(83, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(395, 410);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "sales";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Price";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 224);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(518, 410);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 41);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "stock";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(151, 619);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 41);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1328, 704);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Simple_CRUD_App
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-HFSKL45;Initial Catalog=SchoolDatabase;Integrated Security=True");
+        object name;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            LoadID();
+            LoadSales();            
+        }
+        public void LoadID()
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select Item_id From Stock", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                comboBox1.Items.Add(reader["item_id"].ToString());
+            }   
+            conn.Close();
+        }
+        public void LoadSales()
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * From Sales", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            dataGridView2.DataSource = dataTable;            
+            conn.Close();
+        }
+        
+        
+        private void button1_Click(object sender, EventArgs e)//Add sales btn
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                SqlCommand cmd = new SqlCommand($"SELECT item_name FROM Stock WHERE item_id = {comboBox1.SelectedItem}", conn);
+                conn.Open();
+                name = cmd.ExecuteScalar();
+                conn.Close();
+
+                conn.Open();
+                SqlCommand cmd2 = new SqlCommand("INSERT INTO Sales (item_id, item_name, no_of_items, unit_price) VALUES (" + comboBox1.SelectedItem.ToString() + ",'" + name.ToString() + "'," + textBox1.Text + "," + textBox2.Text + ")", conn);
+                int result = cmd2.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    MessageBox.Show("Added Sales record");
+                }
+                else { MessageBox.Show("Error Adding the record"); }
+                conn.Close();
+
+                conn.Open();
+                SqlCommand cmd3 = new SqlCommand("Update Stock " +
+                    "SET item_quantity = item_quantity - " + textBox1.Text + "" +
+                    "Where item_id = " + comboBox1.SelectedItem.ToString() + "", conn);
+                cmd3.ExecuteNonQuery();
+                conn.Close();
+
+                //to refresh the sales table
+                LoadSales();
+            }
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)//Load sales table
+        {
+            LoadSales();
+        } 
+
+        private void button3_Click(object sender, EventArgs e)//Load Stock DAta btn
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * From Stock", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            dataGridView2.DataSource = table;
+            conn.Close();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)//Delete Sales btn
+        {
+            if (dataGridView2.SelectedRows.Count > 0)
+            {
+                int salesId = Convert.ToInt32(dataGridView2.SelectedRows[0].Cells["sales_id"].Value);
+                int itemId = Convert.ToInt32(dataGridView2.SelectedRows[0].Cells["item_id"].Value.ToString());
+                int itemQuantity = Convert.ToInt32(dataGridView2.SelectedRows[0].Cells["no_of_items"].Value.ToString());
+
+                conn.Open();
+                SqlCommand cmd2 = new SqlCommand("DELETE FROM Sales WHERE sales_id = @salesId", conn);
+                cmd2.Parameters.AddWithValue("@salesId", salesId);
+                int result = cmd2.ExecuteNonQuery();
+
+                conn.Close();
+
+                if (result > 0)
+                {
+                    LoadSales(); // Reload the DataGridView after deletion
+
+                    if (dataGridView2.SelectedRows.Count > 0)
+                    { 
+                        // Update the stock table
+                        conn.Open();
+                        SqlCommand cmd = new SqlCommand("UPDATE Stock SET item_quantity = item_quantity + " + itemQuantity+ " Where item_id = " + itemId+"", conn);
+                        int results = cmd.ExecuteNonQuery();
+                        conn.Close();
+
+                        if (results > 0)
+                        {
+                            MessageBox.Show("Stock quantity restored successfully.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error restoring stock quantity.");
+                        }
+                    }
+
+                    LoadSales();
+                }
+                else
+                {
+                    MessageBox.Show("Error deleting record.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to delete.");
+            }
+        }
+    }
+}
